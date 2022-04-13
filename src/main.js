55c6e233ce621724a6c42cfa0d661d125a6a9a24
nymmm <992747976@qq.com>
2022-04-13 17:17:02 +0800

    change main

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@
  * @Author: Nie Yaoming
  * @Date: 2022-04-13 11:39:41
  * @LastEditors: NieYaoming
- * @LastEditTime: 2022-04-13 14:04:54
+ * @LastEditTime: 2022-04-13 17:16:22
  * @FilePath: \my-practise\src\main.js
  * @Description:
  */
 const show = require('./show');
-show('Webpack');
+show('World');
